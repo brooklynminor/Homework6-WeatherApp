f1563958932507f9ffeb0bcc8464d2355e429195
Brooklyn Minor <brooklynminor4@gmail.com>
2020-11-12 16:29:07 -0500

    using onecall now

--- index.js
+++ index.js
@@ -6,60 +6,61 @@ var weatherIconURL = "https://openweathermap.org/img/wn/10d@2x.png"
 // This is our API key
 var APIkey = "3ab9c663741007177d4cd151315964af";
 
-searchBtn.addEventListener('click', function () {
-  const cityName = document.querySelector('#cityName').value 
-  console.log( `hi there cityName=${cityName}`)
+var searchCity = localStorage.search ? JSON.parse(localStorage.search) : [];
 
-  getWeather( cityName )
+
+searchBtn.addEventListener('click', function () {
+  const cityName = document.querySelector('#cityName').value
+  console.log(`hi there cityName=${cityName}`)
+
+  searchCity.push(cityName)
+  localStorage.search = JSON.stringify(searchCity)
+  displaySearchCity();
+  getWeather(cityName)
 });
 // function search( searchWeather ){
 // $('#search').append(`
 // <button onclick="getWeather('${searchWeather}'">${searchWeather}</button>) 
 // } 
-var searchCityArray =  JSON.parse(localStorage.getItem("searchHistory"));
+var saveName = [];
 
-console.log(searchCityArray)
+function displaySearchCity() {
+  console.log(searchCity)
 
-function displaySearchCity(){
-  console.log( searchCityArray )
+  for (var i = 0; i < searchCity.length; i++) {
+    if (searchCity[i]) {
+      saveName = searchCity[i]
+      document.querySelector('#loadCityName').innerHTML += `<button type="button" class="btn btn-secondary btn-sm" onClick="getWeather('${saveName}')">` + `${saveName}` + '</button>';
 
-  var saveName = [];
-  document.querySelector('#localStorage-card').innerHTML = '<p>' + `${cityName}` + '</p>';
-   for(var i=0; i<searchCityArray.length; i++){
-      if(searchCityArray[i].name ){
-          saveName = searchCityArray[i].name
-      }
-   }
-   document.querySelector('#localStorage-card').textContent = saveName 
+    }
+  }
 }
 
 
 function loadSearchCity() {
   document.getElementById('searchCity')
-  searchCity = localStorage.search ? JSON.parse(localStorage.search) : [];
   //  document.querySelector("#search-list").innerHTML+=`
   //     <button type="button" class="btn btn-warning" onClick="clearAll()">Clear All</button>`
   for (let i = 0; i < searchCity.length; i++) {
     document.querySelector("#searchCity").innerHTML += `
               <button class="btn btn-primary btn-lg" onclick="sideBarSearch('${searchCity[i]}')">${searchCity[i]}</button>`
   }
+
 }
+
 
 // current day, month, date, year and time and updates every second
-function showTime(){
+function showTime() {
   currentDay.innerText = moment().format('dddd, MMMM Do YYYY, h:mm a');
 }
-setInterval( showTime, 1000)
+setInterval(showTime, 1000)
 showTime()
-
-
-
 
 
 
 // current city forcast --- only run WHEN user has clicked and got a CITY NAME
 
-function getWeather( cityName ){
+function getWeather(cityName) {
   var cityNameURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=` + APIkey;
   var apiData
 
@@ -81,74 +82,51 @@ function getWeather( cityName ){
     $.get('https://api.openweathermap.org/data/2.5/uvi?lat=' + currentResponse.coord.lat + '&lon=' + currentResponse.coord.lon + '&units=metric&exclude=daily&appid=' + APIkey)
 
       .then(function (oneCallResponse) {
-        console.log( `oneCallResponse: `, oneCallResponse.value)
-        // console.log(`coords: `, oneCallResponse.coord.lat, oneCallResponse.coord.lon);
+        console.log(`oneCallResponse: `, oneCallResponse.value)
         document.querySelector('.uv').textContent = oneCallResponse.value
 
-        // to convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
       })
 
-      // .catch(function (error) {
-      //   console.log(error)
-      // });
-  })
+      $.get( 'https://api.openweathermap.org/data/2.5/onecall?lat=' + currentResponse.coord.lat + '&lon=' + currentResponse.coord.lon + '&units=metric&appid=' + APIkey)
 
-
-  // 5-day forcast API by city name
-  var fiveDayForcastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=` + APIkey;
-
-  $.ajax({
-    url: fiveDayForcastURL,
-    method: "GET"
-  }).then(function (fiveDayResponse) {
-    // console.log(`response is: `, fiveDayResponse)
-
-    fiveDayResponse.daily.forEach(element => {
-      var displayEl1 = $('<p>' + fiveDayResponse.current.weather[1].description + '</p>');
-      $('#currentWeather-card1').append(displayEl1)
-      // to convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-      document.querySelector('.date1').textContent = moment().add(1, "days").format("MMM Do YYYY");
-      document.querySelector('.humidity1').textContent = 'Humidity: ' + element.main.humidity + '%';
-      document.querySelector('.temp1').textContent = 'Temp: ' + (element.main.temp - 273.15).toFixed(1);
-      $('#icon1').html(`<img class='ml-5' src="https://openweathermap.org/img/wn/${element.weather[1].icon}@2x.png"/>`);
-
-        var displayEl2 = $('<p>' + fiveDayResponse.current.weather[2].description + '</p>');
-        $('#currentWeather-card2').append(displayEl2);
-        // to convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-        document.querySelector('.date2').textContent = moment().add(2, "days").format("MMM Do YYYY");
-        document.querySelector('.humidity2').textConent = 'Humidity: ' + element.main.humidity + '%';
-        document.querySelector('.temp2').textContent = 'Temp: ' + (element.main.temp - 273.15).toFixed(1);
-        $('#icon2').html(`<img class='ml-5' src="https://openweathermap.org/img/wn/${element.weather[2].icon}@2x.png"/>`);
-
-        var displayEl3 = $('<p>' + fiveDayResponse.current.weather[3].description + '</p>');
-        $('#currentWeather-card3').append(displayEl3);
-        // to convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-        document.querySelector('.date3').textContent = moment().add(3, "days").format("MMM Do YYYY");
-        document.querySelector('.humidity3').textContent = 'Humidity: ' + element.main.humidity + '%';
-        document.querySelector('.temp3').textContent = 'Temp: ' + (element.main.temp - 273.15).toFixed(1);
-        $('#icon3').html(`<img class='ml-5' src="https://openweathermap.org/img/wn/${element.weather[3].icon}@2x.png"/>`);
-
-        var displayEl4 = $('<p>' + fiveDayResponse.current.weather[4].description + '</p>');
-        $('#currentWeather-card4').append(displayEl4);
-        // to convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-        document.querySelector('.date4').textContent = moment().add(4, "days").format("MMM Do YYYY");
-        document.querySelector('.humidity4').textContent = 'Humidity: ' + element.main.humidity + '%';
-        document.querySelector('.temp4').textContent = 'Temp: ' + (element.main.temp - 273.15).toFixed(1);
-        $('#icon4').html(`<img class='ml-5' src="https://openweathermap.org/img/wn/${element.weather[4].icon}@2x.png"/>`);
-
-        var displayEl5 = $('<p>' + fiveDayResponse.current.weather[5].description + '</p>');
-        $('#currentWeather-card5').append(displayEl5);
-        // to convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-        document.querySelector('.date5').textContent = moment().add(5, "days").format("MMM Do YYYY");
-        document.querySelector('.humidity5').textContent = 'Humidity: ' + element.main.humidity + '%';
-        document.querySelector('.temp5').textContent = 'Temp: ' + (element.main.temp - 273.15).toFixed(1);
-        $('#icon5').html(`<img class='ml-5' src="https://openweathermap.org/img/wn/${element.weather[5].icon}@2x.png"/>`);
-
+      // 5-day forcast API by city name
+      .then(function (fiveDayResponse) {
+        console.log(`response is: `, fiveDayResponse)
+        element = fiveDayResponse
+          
+          document.querySelector('#date1').textContent = moment().add(1, "days").format("MMM Do YYYY");
+          document.querySelector('.humidity1').textContent = 'Humidity: ' + element.daily[1].humidity + '%';
+          document.querySelector('.temp1').textContent = 'Temp: ' + (element.daily[1].temp.day - 273.15).toFixed(1);
+          $('#icon1').html(`<img class='ml-4' src="https://openweathermap.org/img/wn/${element.daily[1].weather[0].icon}@2x.png"/>`);
+    
+          document.querySelector('#date2').textContent = moment().add(2, "days").format("MMM Do YYYY");
+          document.querySelector('.humidity2').textConent = 'Humidity: ' + element.daily[2].humidity + '%';
+          document.querySelector('.temp2').textContent = 'Temp: ' + (element.daily[2].temp.day - 273.15).toFixed(1);
+          $('#icon2').html(`<img class='ml-4' src="https://openweathermap.org/img/wn/${element.daily[2].weather[0].icon}@2x.png"/>`);
+    
+          document.querySelector('#date3').textContent = moment().add(3, "days").format("MMM Do YYYY");
+          document.querySelector('.humidity3').textContent = 'Humidity: ' + element.daily[3].humidity + '%';
+          document.querySelector('.temp3').textContent = 'Temp: ' + (element.daily[3].temp.day - 273.15).toFixed(1);
+          $('#icon3').html(`<img class='ml-4' src="https://openweathermap.org/img/wn/${element.daily[3].weather[0].icon}@2x.png"/>`);
+    
+          document.querySelector('#date4').textContent = moment().add(4, "days").format("MMM Do YYYY");
+          document.querySelector('.humidity4').textContent = 'Humidity: ' + element.daily[4].humidity + '%';
+          document.querySelector('.temp4').textContent = 'Temp: ' + (element.daily[4].temp.day - 273.15).toFixed(1);
+          $('#icon4').html(`<img class='ml-4' src="https://openweathermap.org/img/wn/${element.daily[4].weather[0].icon}@2x.png"/>`);
+    
+          document.querySelector('#date5').textContent = moment().add(5, "days").format("MMM Do YYYY");
+          document.querySelector('.humidity5').textContent = 'Humidity: ' + element.daily[5].humidity + '%';
+          document.querySelector('.temp5').textContent = 'Temp: ' + (element.daily[5].temp.day - 273.15).toFixed(1);
+          $('#icon5').html(`<img class='ml-4' src="https://openweathermap.org/img/wn/${element.daily[5].weather[0].icon}@2x.png"/>`);
+    
       
-    })
+          })
+          .catch(function (error) {
+            console.log(error)
+      
+        })
+  
+  })
+  
 
-      .catch(function (error) {
-        console.log(error)
-      })
-    })
 }
